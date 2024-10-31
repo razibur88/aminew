5ce62a6e52080ab2e5c5b27e8bf5b196e88f0b05
razibur88 <razibur888@gmail.com>
2024-10-31 19:20:49 +0600

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+
 import About from "./components/about/About"
 import Banner from "./components/banner/Banner"
 import Footer from "./components/footer/Footer"
-import Header from "./components/header/header"
 
 
 
 function App() {
   return (
   <>
-     <Header/>
+     
      <Banner/>
      <About/>
      <Footer/>
